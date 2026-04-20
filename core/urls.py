af2953422b84074dc71913dde1dd9e1eb66a99a7
souhajuili-lang <souha.juili@isimg.com>
2026-04-21 01:50:34 +0200

    Ajout de la page secrète admin

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
-# --- ADD THESE TWO NEW IMPORTS ---
-from django.conf import settings
-from django.conf.urls.static import static
+# --- NOTRE SCRIPT SECRET ---
+def create_admin(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@admin.com', 'TonMotDePasse123')
+        return HttpResponse("<h1>🎉 VICTOIRE ! Superuser créé avec succès !</h1> <p>Tu peux maintenant aller sur /admin/</p>")
+    return HttpResponse("<h1>👍 Le superuser existe déjà !</h1> <p>Va te connecter sur /admin/</p>")
+# ---------------------------
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls')),
+    path('setup-admin/', create_admin),  # <-- Le lien secret
 ]
-
-# --- ADD THIS SECURITY RULE AT THE BOTTOM ---
-# This tells Django to serve our image files, but only while we are developing locally
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
